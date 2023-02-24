f900939504a50ef0fcb113f477e917a7f6a07d9d
umang0620 <120244531+umang0620@users.noreply.github.com>
2023-02-24 22:21:12 +0530

    Day-1 Task

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello From Me</h1>
+        <p> Umang Mistry</p>
+    </div>
+  );
+}
+
+export default App;
